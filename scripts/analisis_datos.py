288f2c427aae8cbd3d815b87c6e3f14fd175f2a4
Yael Martin <yaelmartin1@gmail.com>
2026-05-09 19:13:57 +0000

    PROY-2: Agregar análisis de ventas

---
+++ scripts/analisis_datos.py
@@ -0,0 +1,24 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("datos/ventas.csv")
+
+df["total"] = df["cantidad"] * df["precio"]
+
+ventas_totales = df["total"].sum()
+
+producto_mas_vendido = df.groupby("producto")["cantidad"].sum().idxmax()
+
+print("Ventas totales:", ventas_totales)
+print("Producto más vendido:", producto_mas_vendido)
+
+ventas_por_producto = df.groupby("producto")["total"].sum()
+
+ventas_por_producto.plot(kind="bar")
+
+plt.title("Ventas por Producto")
+
+plt.savefig("resultados/grafico_ventas.png")
+
+plt.show()
